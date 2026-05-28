391940e256dec91dbd19ac680e08da36296bd95c
Chongze Peng <chongzepeng03@gmail.com>
2026-05-28 11:49:56 -0400

    Add: implement-prefix-tree - submission-33

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-33.py
@@ -0,0 +1,38 @@
+class trieNode:
+    def __init__(self):
+        self.children = {}
+        self.isLastc = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = trieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if not cur.children.get(c):
+                cur.children[c] = trieNode()
+            cur = cur.children[c]
+        cur.isLastc = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            # if not cur.children.get(c):
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.isLastc
+        
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            # if not cur.children.get(c):
+            if c not in cur.children:
+
+                return False
+            cur = cur.children[c]
+        return True 
+        
