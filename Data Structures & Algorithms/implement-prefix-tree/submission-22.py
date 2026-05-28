aa1c61b13e108a59292f1001450b5990d2ab3cd8
Chongze Peng <chongzepeng03@gmail.com>
2026-05-28 00:50:09 -0400

    Add: implement-prefix-tree - submission-22

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-22.py
@@ -0,0 +1,22 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.wordList = set()
+
+
+    def insert(self, word: str) -> None:
+        self.wordList.add(word)
+        # for i in range(1, len(word)-1):
+        #     self.prefix.add(word[0:i])
+
+
+    def search(self, word: str) -> bool:
+        return word in self.wordList
+
+    def startsWith(self, prefix: str) -> bool:
+        for word in self.wordList:
+            # if prefix == word[:len(prefix)]:
+            if word.startswith(prefix):
+                return True
+        return False
+        
